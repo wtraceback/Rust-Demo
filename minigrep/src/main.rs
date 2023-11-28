b934229cea4ef4a92fb6d6a095d2cca78bce50a2
whxcer <whxcer@outlook.com>
2023-11-28 23:18:06 +0800

    【minigrep】重构：改善模块化

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,21 +1,31 @@
-use std::env;       // 使用 Rust 标准库提供的 std::env::args 函数获取传递给 minigrep 的命令行参数迭代器
+use std::env;
 use std::fs;
 
 fn main() {
-    // 使用 collect 函数将迭代器转换成一个包含所有迭代器产出值的动态数组
-    // 运行：$ cargo run test test.name
-    // 输出：["target\\debug\\minigrep.exe", "test", "test.name"]
     let args: Vec<String> = env::args().collect();
 
-    // args[0] 为当前执行的二进制文件名称，即 target/debug/minigrep.exe
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
+    println!("query = {}", config.query);
+    println!("filename = {}", config.filename);
+    println!();
 
-    println!("query = {}", query);
-    println!("filename = {}", filename);
-
-    let contents = fs::read_to_string("poem.txt")
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Self {
+        // args[0] 为当前执行的二进制文件名称，即 target/debug/minigrep.exe
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
